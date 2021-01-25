48e52f649f0d4f3fd70ac8362e13097e9784f20f
Shantanu Singh <sshantanuthakur1099@gmail.com>
2021-01-25 19:33:47 +0530

    Updated changes

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const nav = document.querySelector(".toggleNavBar");
 const navLinks = document.querySelector("nav_links");
 
 nav.addEventListener('click', (event) => {
-if(event.which){
-    document.body.navLinks.style.display = block;
-    event.which = 0;
-}
-else{
-    navLinks.style.display = none;
-}
+// if(event.which){
+//     document.body.navLinks.style.display = block;
+//     event.which = 0;
+// }
+// else{
+//     navLinks.style.display = none;
+// }
+console.log(event.which);
 });
